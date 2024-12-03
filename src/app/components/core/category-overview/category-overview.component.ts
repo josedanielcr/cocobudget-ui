f81032070071e185c1a41ce240232a457181d4dc
josedanielcr <jdcanales1905@gmail.com>
2024-12-02 23:21:23 -0600

    Feat: category overview

--- src/app/components/core/category-overview/category-overview.component.ts
+++ src/app/components/core/category-overview/category-overview.component.ts
@@ -2,6 +2,7 @@ import { Component, Input} from '@angular/core';
 import {Category} from '../../../models/Category';
 import {CreateCategoryComponent} from '../create-category/create-category.component';
 import {CategoryLearnInfoComponent} from '../../shared/category-learn-info/category-learn-info.component';
+import {CurrencyPipe} from '@angular/common';
 
 @Component({
   selector: 'app-category-overview',
@@ -11,13 +12,18 @@ import {CategoryLearnInfoComponent} from '../../shared/category-learn-info/categ
     CategoryLearnInfoComponent
   ],
   templateUrl: './category-overview.component.html',
-  styleUrl: './category-overview.component.css'
+  styleUrl: './category-overview.component.css',
+  providers: [CurrencyPipe]
 })
 export class CategoryOverviewComponent {
 
   @Input() categories : Category[] | undefined = [];
   @Input() folderId : string | undefined;
 
-  constructor() {
+  constructor(private currencyPipe : CurrencyPipe) {
+  }
+
+  formatCurrency(amountRemaining: number) {
+    return this.currencyPipe.transform(amountRemaining, '', '', '1.2-2') || '';
   }
 }
