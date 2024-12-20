ac0522d07ffc4f056fd5e0051bf300584c312a6a
josedanielcr <jdcanales1905@gmail.com>
2024-12-19 22:02:03 -0600

    Feat: template to show bank account transactions (not implemented yet)

--- src/app/components/core/transactions/bank-account-transactions-overview/bank-account-transactions-overview.component.ts
+++ src/app/components/core/transactions/bank-account-transactions-overview/bank-account-transactions-overview.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {BankAccount} from '../../../../models/BankAccount';
 
 @Component({
   selector: 'app-bank-account-transactions-overview',
@@ -9,4 +10,5 @@ import { Component } from '@angular/core';
 })
 export class BankAccountTransactionsOverviewComponent {
 
+  @Input() bankAccount : BankAccount | undefined;
 }
